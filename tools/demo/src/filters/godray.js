ac621b0285dcc3ad602e77130ae27de15f6c29e5
Rocky Wei <finscn@gmail.com>
2017-10-26 00:09:04 +0800

    Refactor `GodrayFilter` for optimizing.

--- tools/demo/src/filters/godray.js
+++ tools/demo/src/filters/godray.js
@@ -1,8 +1,24 @@
 export default function() {
-    this.addFilter('GodrayFilter', function(folder) {
-        folder.add(this, 'time', 0, 1);
-        folder.add(this, 'angle', -60, 60);
-        folder.add(this, 'gain', 0, 1);
-        folder.add(this, 'lacunarity', 0, 5);
+    const app = this;
+
+    this.addFilter('GodrayFilter', {
+        enabled: true,
+        opened: true,
+        oncreate: function(folder) {
+            const filter = this;
+
+            this.angle = 30;
+            this.gain = 0.7;
+            this.lacunarity = 3;
+
+            app.events.on('animate', function(delta){
+                filter.time += delta / 1000.0 * 0.5;
+            });
+
+            folder.add(this, 'time', 0, 1);
+            folder.add(this, 'angle', -60, 60);
+            folder.add(this, 'gain', 0, 1);
+            folder.add(this, 'lacunarity', 0, 5);
+        }
     });
 }
