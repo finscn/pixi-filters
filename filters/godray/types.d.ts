8d3ef9c8cd7a8460fa53a38436476420dcc10c1d
finscn <finscn@gmail.com>
2017-12-24 03:06:01 +0800

    update .ts.d

--- filters/godray/types.d.ts
+++ filters/godray/types.d.ts
@@ -2,7 +2,7 @@
 declare namespace PIXI.filters {
     class GodrayFilter extends PIXI.Filter<{}> {
         constructor(light?:number|PIXI.Point|Array<number>, gain?:number, lacunarity?:number, time?:number);
-        light:number;
+        light:number|PIXI.Point|Array<number>;
         gain:number;
         lacunarity:number;
         time:number;
