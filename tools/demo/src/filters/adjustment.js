73325a5b04241c82222c57cd36916b14c02ebbd6
finscn <finscn@gmail.com>
2017-12-29 07:57:50 +0800

    update

--- tools/demo/src/filters/adjustment.js
+++ tools/demo/src/filters/adjustment.js
@@ -5,7 +5,6 @@ export default function() {
         global: false,
         opened: false,
         fishOnly: true,
-        // args: [[app.initWidth / 2, app.initHeight / 2]],
         oncreate(folder) {
             folder.add(this, 'gamma', 0, 10);
             folder.add(this, 'saturation', 0, 10);
@@ -15,14 +14,6 @@ export default function() {
             folder.add(this, 'green', 0, 10);
             folder.add(this, 'blue', 0, 10);
             folder.add(this, 'alpha', 0, 1);
-            // folder.add(this, 'noise', 0, 1);
-            // folder.add(this, 'noiseSize', 1, 10);
-            // folder.add(this, 'scratch', -1, 1);
-            // folder.add(this, 'scratchDensity', 0, 1);
-            // folder.add(this, 'scratchWidth', 1, 20);
-            // folder.add(this, 'vignetting', 0, 1);
-            // folder.add(this, 'vignettingAlpha', 0, 1);
-            // folder.add(this, 'vignettingBlur', 0, 1);
         }
     });
 }
