dcdc0f80be3d4a9f8c3a805cadcb89bb2f7a73e1
finscn <finscn@gmail.com>
2018-01-10 01:19:42 +0800

    cosmetic

--- tools/demo/src/filters/advanced-bloom.js
+++ tools/demo/src/filters/advanced-bloom.js
@@ -5,10 +5,5 @@ export default function() {
         folder.add(this, 'brightness', 0.5, 1.5);
         folder.add(this, 'blur', 0, 20);
         folder.add(this, 'quality', 0, 20);
-
-        // this.kawase = {
-        //     kernels : [3, 2, 1],
-        //     pixelSize : [1, 1],
-        // };
     });
 }
